3fd43cd42ce59e1ab595026b80966099432357a9
Edward Loveall <edward@edwardloveall.com>
2014-04-14 21:47:36 -0400

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Create temporary edward loveall user
+user_params = {email: 'edward@edwardloveall.com', password: '123', admin: true}
+Monban.config.sign_up_service.new(user_params).perform
