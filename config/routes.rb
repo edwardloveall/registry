4a5ef968277f8ebc13cf83659222682a97d9266f
Edward Loveall <edward@edwardloveall.com>
2014-04-14 21:32:17 -0400

    Removed 'show' route from products

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create]
   namespace :registry do
     root to: 'products#index'
-    resources :products
+    resources :products, except: :show
   end
 
   get 'sign_in', to: 'sessions#new', as: 'sign_in'
